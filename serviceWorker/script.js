22f72060a8f7a9dd16c46f9ceb4fe384b95215bb
etakouer <hoctac@gmail.com>
2023-02-14 08:16:53 +0100

    test blob

--- serviceWorker/script.js
+++ serviceWorker/script.js
@@ -1,8 +1,15 @@
-if ( "serviceWorker" in navigator ) {
-    navigator.serviceWorker.register( "./serviceworker.min.js" );
+var workerfunc = function () {
+	console.log('do something')
 }
-document.addEventListener('DOMContentLoaded', ()=> {
-	document.querySelector('#btn').addEventListener('click', ()=>{
+var blobURL = URL.createObjectURL(new Blob(
+	["(" + workerfunc.toString() + ")"],
+	{ type: 'text/javascript' }))
+
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker.register(blobURL);
+}
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('#btn').addEventListener('click', () => {
 		console.log("click")
 		fetch("/addToCart", {
 			method: "POST"
