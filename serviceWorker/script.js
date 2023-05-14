a08467308bf9653e08bed1f1a1f4779b8cfc9e85
etakouer <hoctac@gmail.com>
2023-05-14 18:08:30 +0200

    test worker

--- serviceWorker/script.js
+++ serviceWorker/script.js
@@ -1,12 +1,13 @@
+/*
 var workerfunc = function () {
 	console.log('do something')
 }
 var blobURL = URL.createObjectURL(new Blob(
 	["(" + workerfunc.toString() + ")"],
 	{ type: 'text/javascript' }))
-
+*/
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(blobURL);
+	navigator.serviceWorker.register("./serviceworker.min.js");
 }
 document.addEventListener('DOMContentLoaded', () => {
 	document.querySelector('#btn').addEventListener('click', () => {
